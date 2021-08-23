93d79cf072599a60495c31689193ec5e5b466428
Liz Ireland <liz.t.ireland@gmail.com>
2021-08-22 19:03:47 -0700

    adding timer to js

--- HomeworkJS.js
+++ HomeworkJS.js
@@ -1,4 +1,4 @@
-var wordBlank = document.querySelector(".word-blanks");
+// var wordBlank = document.querySelector(".word-blanks");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
@@ -11,3 +11,32 @@ var loseCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
+
+var questions = ["who was the green river killer?" , "who was the co-ed killer?" , "Where was Ted Bundy executed?"]
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < -1) {
+            timer = -1;
+        return "Time out!! :("
+            // timer = duration; // uncomment this line to reset timer automatically after reaching 0
+        }
+    }, 1000);
+}
+
+
+window.onload = function () {
+    var time = 60 / 2, 
+        display = document.querySelector('#GameTimer');
+    startTimer(time, display);
+    
+};
