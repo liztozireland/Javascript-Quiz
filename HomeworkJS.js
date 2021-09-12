b5318d82793807bda7f87db5bb1334fb493e0458
Liz Ireland <liz.t.ireland@gmail.com>
2021-09-11 21:14:48 -0700

    updating timer functions

--- HomeworkJS.js
+++ HomeworkJS.js
@@ -1,32 +1,48 @@
-var wordQuestion = document.getElementById(".quiz-question");
+var quizQuestions = document.getElementById(".quiz-question");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var timeEl = document.querySelector(".time");
-
-var chosenWord = "";
-var numBlanks = 0;
-var winCounter = 0;
-var loseCounter = 0;
-var isWin = false;
-var timer;
 var timerCount;
 
-var questions = ["who was the green river killer?" , "who was the co-ed killer?" , "Where was Ted Bundy executed?"]
-var answers = ["Gary Ridgeway","Edmund Kemper", "Florida"];
+
+var quizQuestions = ["who was the green river killer?" , "who was the co-ed killer?" , "Where was Ted Bundy executed?"]
+var quizAnswers = ["Gary Ridgeway","Edmund Kemper", "Florida"];
 
 function init() {
   getWins();
   getlosses();
 }
 
-function startGame() {
+startButton.onclick = function startGame() {
   isWin = false;
   timerCount = 10;
   startButton.disabled = true;
-  renderQuestions()
+  // renderQuestions()
   startTimer()
+}
+
+function startTimer() {
+  // Sets timer
+  timer = setInterval(function() {
+    timerCount--;
+    timeEl.textContent = timerCount;
+    if (timerCount >= 0) {
+      // Tests if win condition is met
+      if (isWin && timerCount > 0) {
+        // Clears interval and stops timer
+        clearInterval(timer);
+        winGame();
+      }
+    }
+    // Tests if time has run out
+    if (timerCount === 0) {
+      // Clears interval
+      clearInterval(timer);
+      // loseGame();
+    }
+  }, 1000);
 }
 
 function winGame() {
@@ -37,27 +53,32 @@ function winGame() {
 }
 
 // The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
+// function loseGame() {
+//   wordBlank.textContent = "GAME OVER";
+//   loseCounter++
+//   startButton.disabled = false;
+//   setLosses()
+// }
 
-var secondsLeft = 10;
 
-startButton.onclick = function setTime() {
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left in game!";
-  
-      if(secondsLeft === 0) {
-        clearInterval(timerInterval);
-        timeEl.textContent = "Time's out! 😢";
+// startButton.addEventListener("click", quizQuestions);
 
-      }
+// startButton.onclick = function showQuestion() {
+//   var quizQuestions = quizQuestions.textContent = "something"
+// }
+
+// startButton.onclick = function setTime() {
+//     var timerInterval = setInterval(function() {
+//       secondsLeft--;
+//       timeEl.textContent = secondsLeft + " seconds left in game!";
   
-    }, 1000);
-  }
+//       if(secondsLeft === 0) {
+//         clearInterval(timerInterval);
+//         timeEl.textContent = "Time's out! 😢";
+
+//       }
+//     }, 1000);
+//   }
   
-  setTime();
+  // setTime();
+
