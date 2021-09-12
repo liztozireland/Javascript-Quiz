8dcf40e3e996407f5202847e53580de29f4af785
Liz Ireland <liz.t.ireland@gmail.com>
2021-09-12 14:37:47 -0700

    adding start game and start timer functions

--- HomeworkJS.js
+++ HomeworkJS.js
@@ -1,7 +1,33 @@
-var wordBlank = document.querySelector(".word-blanks");
+var myQuestins = document.querySelector(".word-blanks");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
-var timerElement = document.querySelector(".timer-count");
+var timeEl = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
-
+var resetButton = document.querySelector(".reset-button");
 //starting over
+
+var questions = ["variable","array", "modulus", "object", "function", "string", "boolean"];
+
+function init() {
+  getWins();
+  getlosses();
+}
+
+function startGame() {
+  isWin = false;
+  timerCount = 10;
+  // Prevents start button from being clicked when round is in progress
+  startButton.disabled = true;
+  showQuestions()
+  startTimer()
+}
+
+
+function showQuestions() {
+
+}
+function startTimer() {
+  
+}
+
+startButton.addEventListener("click", startGame);
