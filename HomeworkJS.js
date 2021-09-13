5549c96061aa8a8e506d8545636e2706af4a524e
Liz Ireland <liz.t.ireland@gmail.com>
2021-09-12 17:04:00 -0700

    creating containers

--- HomeworkJS.js
+++ HomeworkJS.js
@@ -1,26 +1,37 @@
-var myQuestions = document.querySelector(".my-questions");
+var questionContainer = document.querySelector(".question-container");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var timeEl = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
 var resetButton = document.querySelector(".reset-button");
+var questionClass = document.querySelector(".question-deck")
 
-var chosenQuestion = "";
-var numBlanks = 0;
+var someQuestion = "";
+var someAnswer = "";
 var winCounter = 0;
 var loseCounter = 0;
 var isWin = false;
 var timer;
 var timerCount;
 
-var startQuestion = [];
+var question = [];
 var blanksLetters = [];
 
 
-var questions = ["how are you?" , "question 2", "question 3"]
-// var question1Array = ["answer 1", "answer 1", "answer 3"]
-// var question2Array = ["answers 1", "answerd 1", "answerl 3"]
-// var answerArray = ["answer 1", "answerd 1", "answer 3"]
+// var question1 =  [
+// 	{
+// 		question: "What is 10/2?",
+// 		answers: {
+// 			a: '3',
+// 			b: '5',
+// 			c: '115'
+// 		},
+// 		correctAnswer: 'b'
+// 	}
+// ];
+var question1Array = ["answer 1", "answer 1", "answer 3"]
+var question2Array = ["answers 1", "answerd 1", "answerl 3"]
+var answerArray = ["answer 1", "answerd 1", "answer 3"]
 
 var i = 0
 while (i<5) { i++}
@@ -65,7 +76,15 @@ function startTimer() {
 
 function startQuestions() {
   console.log("hello")
-  myQuestions.textContent = "how are you?";
+  questionContainer.textContent = question1Array[0];
+      for (var i = 0; i < question1Array.length; i++) {
+        // var createquestionClassEl = document.createElement("div")
+        // createquestionClassEl.classList.add("question-card");
+        // // var questionData = document.createElement("p");
+        // // var questionText = document.createElement("h4");
+        // textContent = question1Array[0];
+        // createquestionClassEl.appendChild(text);
+      }
 }
 
 startButton.addEventListener("click", startGame);
